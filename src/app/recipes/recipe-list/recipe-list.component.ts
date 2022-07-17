a56cd81caf606d886ac1dd84c5a16b981511fb07
Patryk Szaruga <szarugapatryk@gmail.com>
2022-07-17 15:06:12 +0200

    Add sample element for recipes array

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Recipe} from "../recipe.model";
 
 @Component({
@@ -7,9 +7,17 @@ import {Recipe} from "../recipe.model";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    {
+      name: 'A test Recipe', description: 'This is simply a test',
+      imagePath: 'https://images.unsplash.com/photo-1495521821757-' +
+                 'a1efb6729352?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG' +
+                 '90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1926&q=80'
+    },
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
