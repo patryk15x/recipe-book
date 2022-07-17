44655bfc75b1bff617fab42ea68c6660fd42b9c0
Patryk Szaruga <szarugapatryk@gmail.com>
2022-07-17 13:21:18 +0200

    Remove non-existing component from app.module.ts, register ShoppingEditComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import {RecipeDetailComponent} from "./recipes/recipe-detail/recipe-detail.component";
 import {RecipeListComponent} from "./recipes/recipe-list/recipe-list.component";
 import {RecipeItemComponent} from "./recipes/recipe-list/recipe-item/recipe-item.component";
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
@@ -19,8 +19,8 @@ import {RecipeItemComponent} from "./recipes/recipe-list/recipe-item/recipe-item
     RecipeListComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingListEditComponent,
-    RecipesComponent
+    RecipesComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
